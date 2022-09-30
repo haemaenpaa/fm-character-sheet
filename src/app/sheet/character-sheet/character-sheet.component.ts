7b6d6cca791bc892b7d49f6826f2c0f6cabc4dac
haemaenpaa <heikki.a.maenpaa@gmail.com>
2022-09-30 19:34:49 +0300

    Added name, race and AOs to the character sheet view.

--- src/app/sheet/character-sheet/character-sheet.component.ts
+++ src/app/sheet/character-sheet/character-sheet.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Character } from 'src/app/model/character';
 
+interface LevelStruct {
+  abilityOrigin: string;
+  level: number;
+}
+
 @Component({
   selector: 'character-sheet',
   templateUrl: './character-sheet.component.html',
@@ -11,4 +16,13 @@ export class CharacterSheetComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  get levelList(): LevelStruct[] {
+    var ret: LevelStruct[] = [];
+    const aoLevels = this.character.aoLevels;
+    for (let ao in aoLevels) {
+      ret.push({ abilityOrigin: ao, level: aoLevels[ao] });
+    }
+    return ret;
+  }
 }
--- src/app/sheet/sheet.module.ts
+++ src/app/sheet/sheet.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { AbilityScoreComponent } from './ability-score/ability-score.component';
 import { AbilityGridComponent } from './ability-grid/ability-grid.component';
 import { CharacterSheetComponent } from './character-sheet/character-sheet.component';
+import { AbilityNamePipe } from './pipe/ability-name.pipe';
 
 @NgModule({
   declarations: [
     AbilityGridComponent,
     AbilityScoreComponent,
     CharacterSheetComponent,
+    AbilityNamePipe,
   ],
   imports: [CommonModule],
   exports: [CharacterSheetComponent, AbilityGridComponent],
