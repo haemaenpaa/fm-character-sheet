40f6f75fe949e9486483eb63464b5f1bc9c50da5
haemaenpaa <heikki.a.maenpaa@gmail.com>
2022-11-25 18:17:10 +0200

    Moved character builder to its own file.

--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Character, CharacterBuilder } from '../model/character';
+import { Character } from '../model/character';
+import { CharacterBuilder } from '../model/character-builder';
 
 /**
  * An injectable service that manages the character information.
