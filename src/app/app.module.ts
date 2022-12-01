a53175c26fdea6c71d45aaff806e3b9a1233f3ed
haemaenpaa <heikki.a.maenpaa@gmail.com>
2022-12-01 15:45:42 +0200

    Added @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SheetModule } from './sheet/sheet.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 /**
  * The main application module.
  */
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SheetModule],
+  imports: [BrowserModule, AppRoutingModule, SheetModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
