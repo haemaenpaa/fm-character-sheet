ff997f254263543eb5afb66b96833ff2318efd9e
haemaenpaa <heikki.a.maenpaa@gmail.com>
2022-12-22 23:02:49 +0200

    Character List component draft.

---
+++ src/app/character-list/character-li/character-li.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { LevelStruct, levelStructs } from 'src/app/common/LevelStruct';
+import Character from 'src/app/model/character';
+
+@Component({
+  selector: 'character-li',
+  templateUrl: './character-li.component.html',
+  styleUrls: ['./character-li.component.css'],
+})
+export class CharacterLiComponent {
+  @Input() character: Character | null = null;
+  get levelList() {
+    return this.character?.getAoLevels();
+  }
+  get levels(): LevelStruct[] {
+    if (!this.character) {
+      return [];
+    }
+    return levelStructs(this.character);
+  }
+}
