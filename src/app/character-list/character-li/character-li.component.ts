da399b5f224940a72fa52b441c8f4804722ff9e3
haemaenpaa <heikki.a.maenpaa@gmail.com>
2022-12-23 22:14:15 +0200

    Some cosmetic improvements to character list and resistance item.

--- src/app/character-list/character-li/character-li.component.ts
+++ src/app/character-list/character-li/character-li.component.ts
@@ -2,6 +2,16 @@ import { Component, Input } from '@angular/core';
 import { LevelStruct, levelStructs } from 'src/app/common/LevelStruct';
 import Character from 'src/app/model/character';
 
+function abbreviate(value: string): string {
+  if (value.split(' ').length > 1) {
+    return value.split(' ').reduce((out: string, cur: string) => {
+      return out + cur.charAt(0).toUpperCase();
+    }, '');
+  }
+
+  return value.substring(0, 3);
+}
+
 @Component({
   selector: 'character-li',
   templateUrl: './character-li.component.html',
@@ -17,7 +27,10 @@ export class CharacterLiComponent {
     if (!this.character) {
       return [];
     }
-    return levelStructs(this.character);
+    return levelStructs(this.character).map((ls) => ({
+      ...ls,
+      abilityOrigin: abbreviate(ls.abilityOrigin),
+    }));
   }
   onMouseEnter() {
     this.hovered = true;
