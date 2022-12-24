74a9e2c85766eddfa2cbb32d6baeb0905a052876
haemaenpaa <heikki.a.maenpaa@gmail.com>
2022-12-24 22:44:07 +0200

    Added random name generation.

--- src/app/character-list/character-list.component.ts
+++ src/app/character-list/character-list.component.ts
@@ -1,7 +1,65 @@
 import { Component } from '@angular/core';
+import { last } from 'rxjs';
 import Character from '../model/character';
 import { CharacterBuilder } from '../model/character-builder';
 import { CharacterService } from '../services/character.service';
+
+const FIRST_NAMES = [
+  'Abrax',
+  'Bertrude',
+  'Calin',
+  'Deem',
+  'Eldia',
+  'Fara',
+  'Gort',
+  'Hania',
+  'Illie',
+  'Jeo',
+  'Karl',
+  'Lomen',
+  'Mugg',
+  'Nobb',
+  'Olivia',
+  'Prim',
+  'Qaa',
+  'Rica',
+  'Sanaa',
+  'Tannim',
+  'Ulfbert',
+  'Vic',
+  'Wrenna',
+  'Xill',
+  'Yvon',
+  'Zhop',
+];
+const LAST_NAMES = [
+  'Armiger',
+  'Black',
+  'Carmine',
+  'Deerhart',
+  'Ellesmere',
+  'Foulger',
+  'Giantsbane',
+  'Hale',
+  'Iaison',
+  'Jetwick',
+  'Kellner',
+  'Loonsby',
+  'Masterson',
+  'Naermere',
+  'Ostrakos',
+  'Pheaton',
+  'Quicktongue',
+  'Richwall',
+  'Stronghammer',
+  'Trout',
+  'Underhill',
+  'Vendix',
+  'Wolfspaw',
+  'Xorondir',
+  'Young',
+  'Zorowich',
+];
 
 @Component({
   selector: 'character-list',
@@ -14,7 +72,12 @@ export class CharacterListComponent {
     return this.characterService.allCharacters;
   }
   newCharacter() {
-    const character = new CharacterBuilder().setName('New character').build();
+    const firstname =
+      FIRST_NAMES[Math.floor(Math.random() * FIRST_NAMES.length)];
+    const lastname = LAST_NAMES[Math.floor(Math.random() * LAST_NAMES.length)];
+    const character = new CharacterBuilder()
+      .setName(`${firstname} ${lastname}`)
+      .build();
     this.characterService.persistCharacter(character);
   }
 }
