1c3945147f85d73e2b4fbd52133c233061f2e725
haemaenpaa <heikki.a.maenpaa@gmail.com>
2023-01-08 17:05:04 +0200

    Unit tests.

--- client/src/app/sheet/hit-dice-display/hit-dice-display.component.spec.ts
+++ client/src/app/sheet/hit-dice-display/hit-dice-display.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { By } from '@angular/platform-browser';
 
 import { HitDiceDisplayComponent } from './hit-dice-display.component';
 
@@ -8,9 +10,9 @@ describe('HitDiceDisplayComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HitDiceDisplayComponent ]
-    })
-    .compileComponents();
+      declarations: [HitDiceDisplayComponent],
+      providers: [{ provide: MatDialog, useValue: {} }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HitDiceDisplayComponent);
     component = fixture.componentInstance;
@@ -19,5 +21,30 @@ describe('HitDiceDisplayComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display hit dice', () => {
+    component.hitDiceMax = {
+      6: 1,
+      8: 2,
+      10: 0,
+      12: 4,
+    };
+    component.hitDiceRemaining = {
+      6: 0,
+      8: 1,
+      10: 0,
+      12: 3,
+    };
+    fixture.detectChanges();
+
+    const d6element = fixture.debugElement.query(By.css('#d6'));
+    expect(d6element.nativeElement.textContent.trim()).toBe('0/1 d6');
+    const d8element = fixture.debugElement.query(By.css('#d8'));
+    expect(d8element.nativeElement.textContent.trim()).toBe('1/2 d8');
+    const d10element = fixture.debugElement.query(By.css('#d10'));
+    expect(d10element).toBeNull();
+    const d12element = fixture.debugElement.query(By.css('#d12'));
+    expect(d12element.nativeElement.textContent.trim()).toBe('3/4 d12');
   });
 });
--- client/src/app/sheet/hit-dice-roller/hit-dice-roller.component.spec.ts
+++ client/src/app/sheet/hit-dice-roller/hit-dice-roller.component.spec.ts
@@ -1,16 +1,58 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import CharacterHitDice from 'src/app/model/character-hit-dice';
+import { GameAction, HitDieParams } from 'src/app/model/game-action';
+import { ActionDispatchService } from 'src/app/services/action-dispatch.service';
+import { EditableTextComponent } from '../editable-text/editable-text.component';
 
-import { HitDiceRollerComponent } from './hit-dice-roller.component';
+import {
+  HitDiceRollerComponent,
+  HitDiceRollerData,
+} from './hit-dice-roller.component';
+
+function placeholder(): HitDiceRollerData {
+  const dice: CharacterHitDice = {
+    6: 0,
+    8: 0,
+    10: 0,
+    12: 0,
+  };
+  return {
+    characterId: 0,
+    hitDiceMax: { ...dice },
+    hitDiceRemaining: { ...dice },
+  };
+}
 
 describe('HitDiceRollerComponent', () => {
   let component: HitDiceRollerComponent;
   let fixture: ComponentFixture<HitDiceRollerComponent>;
+  let result: CharacterHitDice;
+  let roll: GameAction;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HitDiceRollerComponent ]
-    })
-    .compileComponents();
+      declarations: [HitDiceRollerComponent, EditableTextComponent],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: {
+            close: (res: CharacterHitDice) => {
+              result = res;
+            },
+          },
+        },
+        { provide: MAT_DIALOG_DATA, useValue: placeholder() },
+        {
+          provide: ActionDispatchService,
+          useValue: {
+            dispatch: (action: GameAction) => {
+              roll = action;
+            },
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HitDiceRollerComponent);
     component = fixture.componentInstance;
@@ -19,5 +61,87 @@ describe('HitDiceRollerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should regain half', () => {
+    component.hitDiceMax = {
+      6: 1,
+      8: 2,
+      10: 2,
+      12: 0,
+    };
+
+    component.regainHalf();
+    expect(result).not.toBeNull();
+    expect(result[6]).withContext('wrong count of d6').toBe(1);
+    expect(result[8]).withContext('wrong count of d8').toBe(1);
+    expect(result[10]).withContext('wrong count of d10').toBe(0);
+    expect(result[12]).withContext('wrong count of d12').toBe(0);
+  });
+
+  it('should regain one', () => {
+    component.hitDiceMax = {
+      6: 0,
+      8: 0,
+      10: 1,
+      12: 1,
+    };
+
+    component.regainHalf();
+    expect(result).not.toBeNull();
+    expect(result[6]).withContext('wrong count of d6').toBe(0);
+    expect(result[8]).withContext('wrong count of d8').toBe(0);
+    expect(result[10]).withContext('wrong count of d10').toBe(1);
+    expect(result[12]).withContext('wrong count of d12').toBe(0);
+  });
+
+  it('should roll from largest', () => {
+    component.hitDiceMax = {
+      6: 0,
+      8: 1,
+      10: 2,
+      12: 1,
+    };
+    component.hitDiceRemaining = {
+      6: 0,
+      8: 1,
+      10: 2,
+      12: 1,
+    };
+    component.roll(3);
+
+    expect(result[6]).withContext('wrong count of d6').toBe(0);
+    expect(result[8]).withContext('wrong count of d8').toBe(1);
+    expect(result[10]).withContext('wrong count of d10').toBe(0);
+    expect(result[12]).withContext('wrong count of d12').toBe(0);
+
+    const checkParams = roll.params as HitDieParams;
+    expect(checkParams[6]).withContext('wrong count of d6 rolled').toBe(0);
+    expect(checkParams[8]).withContext('wrong count of d8 rolled').toBe(0);
+    expect(checkParams[10]).withContext('wrong count of d10 rolled').toBe(2);
+    expect(checkParams[12]).withContext('wrong count of d12 rolled').toBe(1);
+  });
+
+  it('should roll health', () => {
+    component.hitDiceMax = {
+      6: 3,
+      8: 1,
+      10: 2,
+      12: 1,
+    };
+    component.hitDiceRemaining = {
+      6: 1,
+      8: 1,
+      10: 1,
+      12: 1,
+    };
+    component.rollHealth();
+    expect(result).toBeUndefined();
+
+    const checkParams = roll.params as HitDieParams;
+    expect(checkParams[6]).withContext('wrong count of d6 rolled').toBe(3);
+    expect(checkParams[8]).withContext('wrong count of d8 rolled').toBe(1);
+    expect(checkParams[10]).withContext('wrong count of d10 rolled').toBe(2);
+    expect(checkParams[12]).withContext('wrong count of d12 rolled').toBe(1);
   });
 });
