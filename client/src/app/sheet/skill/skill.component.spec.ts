f28c10ef51a7d96d2fce99256e29e985807c68a7
haemaenpaa <heikki.a.maenpaa@gmail.com>
2023-01-21 17:43:49 +0200

    Unit test for skill modifier calculation.

--- client/src/app/sheet/skill/skill.component.spec.ts
+++ client/src/app/sheet/skill/skill.component.spec.ts
@@ -29,4 +29,38 @@ describe('SkillComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should compute modifier', () => {
+    component.proficiency = 3;
+    component.skill.rank = 0;
+    fixture.detectChanges();
+    expect(component.skillModifier)
+      .withContext('Skill rank 0 should have resulted in skill modifier 0')
+      .toBe(0);
+    component.skill.rank = 1;
+    fixture.detectChanges();
+    expect(component.skillModifier)
+      .withContext('Skill rank 1 should have resulted in skill modifier 2')
+      .toBe(2);
+    component.skill.rank = 2;
+    fixture.detectChanges();
+    expect(component.skillModifier)
+      .withContext('Skill rank 2 should have resulted in skill modifier 3')
+      .toBe(3);
+    component.skill.rank = 3;
+    fixture.detectChanges();
+    expect(component.skillModifier)
+      .withContext('Skill rank 3 should have resulted in skill modifier 5')
+      .toBe(5);
+    component.skill.rank = 4;
+    fixture.detectChanges();
+    expect(component.skillModifier)
+      .withContext('Skill rank 4 should have resulted in skill modifier 6')
+      .toBe(6);
+    component.skill.rank = 5;
+    fixture.detectChanges();
+    expect(component.skillModifier)
+      .withContext('Skill rank 5 should have resulted in skill modifier 8')
+      .toBe(8);
+  });
 });
