f1f39f9dc5d9bdcece30ec64f6433c307607c938
haemaenpaa <heikki.a.maenpaa@gmail.com>
2023-01-28 21:08:18 +0200

    Placeholder get request handlers.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import * as path from "path";
-import { Association } from "sequelize";
 import { Attack } from "./model/attack";
 import { Character } from "./model/character";
 import { CharacterSpellbook, Spell } from "./model/character-spells";
@@ -12,52 +11,47 @@ const port = process.env.PORT || 3000;
 
 const sequelize = initializeSchema("sqlite::memory:");
 
+const characterInclude = [
+  {
+    association: Character.Race,
+    include: [Race.Abilities, Race.Resistances],
+  },
+  Character.Resistances,
+  Character.Selections,
+  Character.HitDice,
+  Character.HitDiceRemaining,
+  {
+    association: Character.Spellbook,
+    include: [
+      {
+        association: CharacterSpellbook.Spells,
+        include: [Spell.Damage, Spell.UpcastDamage],
+      },
+    ],
+  },
+  {
+    association: Character.Attacks,
+    include: [Attack.Damage, Attack.Effect],
+  },
+  {
+    association: Character.Inventory,
+    include: [InventoryContainer.Contents],
+  },
+  Character.Bio,
+  Character.Resources,
+];
 sequelize.sync().then((sql) => {
   const charModel = sql.model("Character");
-  console.log("Model:", charModel, charModel.getAttributes());
-  charModel
-    .create(
-      {
-        id: 1,
-        name: "Placeholder character",
-        resistances: [{ type: "immunity", value: "Fire", category: "damage" }],
-      },
-      {
-        include: [
-          {
-            association: Character.Race,
-            include: [Race.Abilities, Race.Resistances],
-          },
-          Character.Resistances,
-          Character.Selections,
-          Character.HitDice,
-          Character.HitDiceRemaining,
-          {
-            association: Character.Spellbook,
-            include: [
-              {
-                association: CharacterSpellbook.Spells,
-                include: [Spell.Damage, Spell.UpcastDamage],
-              },
-            ],
-          },
-          {
-            association: Character.Attacks,
-            include: [Attack.Damage, Attack.Effect],
-          },
-          {
-            association: Character.Inventory,
-            include: [InventoryContainer.Contents],
-          },
-          Character.Bio,
-          Character.Resources,
-        ],
-      }
-    )
-    .then((chr) => {
-      console.log(chr);
-      chr.dataValues.addSavingThrow;
-    });
+  charModel.create(
+    {
+      id: 1,
+      name: "Placeholder character",
+      resistances: [{ type: "immunity", value: "Fire", category: "damage" }],
+    },
+    {
+      include: characterInclude,
+    }
+  );
 });
 
 const frontendPath =
@@ -65,6 +59,36 @@ const frontendPath =
 
 //Serve the frontend content as static.
 app.use(express.static(frontendPath));
+
+app.get("/api/characters", async (req, res) => {
+  sequelize
+    .model("Character")
+    .findAll({ include: characterInclude })
+    .then((characters) => {
+      res.setHeader("Content-Type", "application/json");
+      res.send(characters.map((c) => c.dataValues));
+    });
+});
+
+app.get("/api/character/:characterId", async (req, res) => {
+  const characterId = Number.parseInt(req.params.characterId);
+  if (isNaN(characterId)) {
+    res.sendStatus(400);
+    return;
+  }
+  sequelize
+    .model("Character")
+    .findOne({ where: { id: characterId }, include: characterInclude })
+    .then((character) => {
+      if (!character) {
+        res.sendStatus(404);
+        return;
+      }
+      //TODO: Map the database model into a data transfer model
+      res.setHeader("Content-Type", "application/json");
+      res.send(character.dataValues);
+    });
+});
 
 /**
  * Route that redirects everything not otherwise routed to the SPA frontend.
