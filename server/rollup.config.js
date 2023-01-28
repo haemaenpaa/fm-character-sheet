176aeaa61d4152bd7f1a63a3fad59c981da5f26e
haemaenpaa <heikki.a.maenpaa@gmail.com>
2023-01-28 23:51:08 +0200

    Use the Rollup bundler for server.

---
+++ server/rollup.config.js
@@ -0,0 +1,15 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
+
+export default {
+  input: "build/compiled/index.js",
+  output: {
+    file: "dist/index.js",
+    format: "cjs",
+    sourcemap: false,
+    minifyInternalExports: true,
+  },
+  plugins: [resolve(), commonjs(), json()],
+  external: ["sequelize", "express"],
+};
