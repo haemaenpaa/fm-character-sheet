ebe39c457d4937a4fd18c8447a58664ce6cd7a88
haemaenpaa <heikki.a.maenpaa@gmail.com>
2023-03-05 18:32:07 +0200

    Roll20 macro for skill checks.

--- client/src/app/sheet/roll-log/skill-check/skill-check.component.ts
+++ client/src/app/sheet/roll-log/skill-check/skill-check.component.ts
@@ -1,19 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { SKILL_DEFAULT_NAME } from 'src/app/model/constants';
-import { Roll, SimpleRoll } from 'src/app/model/diceroll';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { Component, Input } from '@angular/core';
+import { Hoverable } from 'src/app/common/hoverable';
+import { ABILITY_TO_NAME, SKILL_DEFAULT_NAME } from 'src/app/model/constants';
+import { SimpleRoll } from 'src/app/model/diceroll';
+import { toModifier } from 'src/app/utils/modifier-utils';
 
 @Component({
   selector: 'skill-check',
   templateUrl: './skill-check.component.html',
   styleUrls: ['./skill-check.component.css', '../log-row-shared.css'],
 })
-export class SkillCheckComponent implements OnInit {
+export class SkillCheckComponent extends Hoverable {
   @Input('roll') roll!: SimpleRoll;
 
-  constructor() {}
-
-  ngOnInit(): void {}
-
+  constructor(private clipboard: Clipboard) {
+    super();
+  }
   get skillName(): string {
     if (!this.roll.title) {
       return 'UNKNOWN';
@@ -23,5 +25,16 @@ export class SkillCheckComponent implements OnInit {
       return SKILL_DEFAULT_NAME[skillId];
     }
     return skillId;
+  }
+
+  copyMacro() {
+    const abl = this.roll.title!.split('_')[2];
+    const rolls = this.roll.dice.map((d) => `${d.dice}d${d.sides}`).join('+');
+    const mods = this.roll.modifiers
+      .map((m) => `${toModifier(m.value)}`)
+      .join('');
+    this.clipboard.copy(
+      `/me makes a ${this.skillName} (${ABILITY_TO_NAME[abl]}) check : [[${rolls}${mods}]]`
+    );
   }
 }
