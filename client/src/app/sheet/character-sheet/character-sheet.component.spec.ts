6b70a993e8f8356028d194ef8ef18d3081c27175
haemaenpaa <heikki.a.maenpaa@gmail.com>
2023-05-20 13:28:07 +0300

    Added option to auto-copy rolls to clipboard .

--- client/src/app/sheet/character-sheet/character-sheet.component.spec.ts
+++ client/src/app/sheet/character-sheet/character-sheet.component.spec.ts
@@ -5,20 +5,39 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable, Subject } from 'rxjs';
 
 import { CharacterSheetComponent } from './character-sheet.component';
+import { ActionDispatchService } from 'src/app/services/action-dispatch.service';
+import { Roll, RollComponent, SimpleRoll } from 'src/app/model/diceroll';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 describe('CharacterSheetComponent', () => {
   let component: CharacterSheetComponent;
   let fixture: ComponentFixture<CharacterSheetComponent>;
   let paramMap: Observable<ParamMap>;
+  let copied: string[];
+  let dummyRolls: Subject<Roll>;
 
   beforeEach(async () => {
+    dummyRolls = new Subject<Roll>();
     paramMap = new Subject();
+    copied = [];
+    const dummyDispatchService = {
+      rolls: () => dummyRolls.asObservable(),
+    };
+    const dummyClipboard = {
+      copy: (str: string) => {
+        copied.push(str);
+        return true;
+      },
+    };
     await TestBed.configureTestingModule({
       declarations: [CharacterSheetComponent],
       providers: [
         { provide: ActivatedRoute, useValue: { paramMap } },
         { provide: MatDialog, useValue: {} },
         { provide: HttpClient, useValue: {} },
+        { provide: Clipboard, useValue: {} },
+        { provide: ActionDispatchService, useValue: dummyDispatchService },
+        { provide: Clipboard, useValue: dummyClipboard },
       ],
     }).compileComponents();
 
@@ -29,5 +48,24 @@ describe('CharacterSheetComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should push rolls if enabled', () => {
+    component.autoPushRolls = true;
+    const roll = new SimpleRoll();
+    roll.title = 'hit-dice';
+    roll.addDie(new RollComponent(8));
+    dummyRolls.next(roll);
+    expect(copied.length).toBe(1);
+    expect(copied[0]).toContain('d8');
+  });
+
+  it('should not push rolls if disabled', () => {
+    component.autoPushRolls = false;
+    const roll = new SimpleRoll();
+    roll.title = 'hit-dice';
+    roll.addDie(new RollComponent(12));
+    dummyRolls.next(roll);
+    expect(copied.length).toBe(0);
   });
 });
