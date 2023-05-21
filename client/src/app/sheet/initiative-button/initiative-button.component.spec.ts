afb85670be26171bf53f868daa8d38e429f4aca6
haemaenpaa <heikki.a.maenpaa@gmail.com>
2023-05-21 16:39:27 +0300

    Unit tests for initiative-related components.

--- client/src/app/sheet/initiative-button/initiative-button.component.spec.ts
+++ client/src/app/sheet/initiative-button/initiative-button.component.spec.ts
@@ -1,23 +1,64 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InitiativeButtonComponent } from './initiative-button.component';
+import { CharacterBuilder } from 'src/app/model/character-builder';
+import {
+  Advantage,
+  GameAction,
+  InitiativeParams,
+} from 'src/app/model/game-action';
+import { ActionDispatchService } from 'src/app/services/action-dispatch.service';
+import { of } from 'rxjs';
+import { AdvantageResolverService } from 'src/app/services/advantage-resolver.service';
+import { ModifierPipe } from '../pipe/modifier.pipe';
 
 describe('InitiativeButtonComponent', () => {
   let component: InitiativeButtonComponent;
   let fixture: ComponentFixture<InitiativeButtonComponent>;
+  let dispatched: GameAction[];
+  let expectedAdvantage: Advantage = 'none';
 
   beforeEach(async () => {
+    dispatched = [];
+    const dummyActionService = {
+      dispatch: (action: GameAction) => dispatched.push(action),
+    };
+    const dummyResolver = {
+      resolveForEvent: (_: any) => {
+        return expectedAdvantage;
+      },
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [ InitiativeButtonComponent ]
-    })
-    .compileComponents();
+      declarations: [InitiativeButtonComponent, ModifierPipe],
+      providers: [
+        { provide: ActionDispatchService, useValue: dummyActionService },
+        { provide: AdvantageResolverService, useValue: dummyResolver },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(InitiativeButtonComponent);
     component = fixture.componentInstance;
+    component.character = new CharacterBuilder().build();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  for (const advantage of ['none', 'advantage', 'disadvantage']) {
+    it(`should call roll with ${advantage}`, () => {
+      expectedAdvantage = advantage as Advantage;
+      const id = 123;
+      component.character!.id = id;
+      component.callRoll({} as unknown as Event);
+      expect(dispatched.length).toBe(1);
+      const actualAction = dispatched[0];
+      const actualParams = actualAction.params as InitiativeParams;
+      expect(actualAction.type).toBe('initiative');
+      expect(actualParams.characterId).toBe(id);
+      expect(actualParams.advantage).toBe(expectedAdvantage);
+    });
+  }
 });
--- client/src/app/sheet/roll-log/initiative-log/initiative-log.component.spec.ts
+++ client/src/app/sheet/roll-log/initiative-log/initiative-log.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InitiativeLogComponent } from './initiative-log.component';
+import { SimpleRoll } from 'src/app/model/diceroll';
 
 describe('InitiativeLogComponent', () => {
   let component: InitiativeLogComponent;
@@ -8,12 +9,12 @@ describe('InitiativeLogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InitiativeLogComponent ]
-    })
-    .compileComponents();
+      declarations: [InitiativeLogComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(InitiativeLogComponent);
     component = fixture.componentInstance;
+    component.roll = new SimpleRoll();
     fixture.detectChanges();
   });
 
